56587e76371c12bd0e59e1fda0cd30cb12c3c3b5
nhathuycn13 <nhathuycn13@gmail.com>
2016-01-08 03:20:09 +0700

    Revert "Revert "Stack init""
    This reverts commit f3fc75452c62ec4169fd7acb2a851d4004ca693a.

---
+++ Node.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Queue
+{
+    class Node
+    {
+        private Label data;
+        private Node _next;
+        public Node() {
+            this.data = new Label();
+            this._next = null;
+            this.data.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.data.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            //this.data.Location = new System.Drawing.Point(3, 0);
+            this.data.Padding = new System.Windows.Forms.Padding(10);
+            this.data.Size = new System.Drawing.Size(41, 85);
+            this.data.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        }
+        public Node(string d, Node next = null) {
+            this.data = new Label();
+            this._next = next;
+            this.data.Text = d;
+            this.data.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.data.Font = new System.Drawing.Font("Microsoft Sans Serif", 10, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.data.Padding = new System.Windows.Forms.Padding(10);
+            this.data.Size = new System.Drawing.Size(41, 85);
+            this.data.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        }
+        public Label Data
+        {
+            get
+            {
+                return this.data;
+            }
+            set
+            {
+                this.data = value;
+            }
+        }
+        public Node next{
+            get
+            {
+                return this._next;
+            }
+            set
+            {
+                this._next = value;
+            }
+        }
+    }
+}
